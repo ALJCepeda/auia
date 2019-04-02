082a49ac4733e08561b4feeda4e83e0f700f4dd3
Alfred Cepeda <AlfredJCepeda@gmail.com>
2019-04-02 01:31:34 -0400

    Improvements

--- src/services/writePlaybook.ts
+++ src/services/writePlaybook.ts
@@ -7,20 +7,29 @@ const writeYAML = require('write-yaml');
 export async function writePlaybook(playbook:AnsiblePlaybook) {
   console.debug('Writing Ansible playbooks');
   
-  await new Promise((resolve, reject) => rimraf('ansible', (err) => {
-    if(err) reject(err);
-    resolve();
-  }));
-  
-  await new Promise((resolve, reject) => fs.mkdir('ansible', (err) => {
-    if(err) reject(err);
-    resolve();
-  }));
-  
-  await new Promise((resolve, reject) => writeYAML('ansible/users.yaml', playbook.users, (err:any) => {
-    if(err) reject(err);
-    resolve();
-  }));
+  await remakeDirectory('ansible');
+  await writePlaybooks('ansible', playbook);
   
   console.debug('Finished writing Ansible playbooks');
+}
+
+async function writePlaybooks(dir:string, playbook:AnsiblePlaybook) {
+  await new Promise((resolve, reject) => writeYAML(`${dir}/users.yaml`, playbook.users, (err:any) => {
+    if(err) reject(err);
+    resolve();
+  }));
+}
+
+async function remakeDirectory(dir:string) {
+  console.debug(`Deleting ${dir}`);
+  await new Promise((resolve, reject) => rimraf(dir, (err) => {
+    if(err) reject(err);
+    resolve();
+  }));
+ 
+  console.debug(`Making ${dir}`);
+  await new Promise((resolve, reject) => fs.mkdir(dir, (err) => {
+    if(err) reject(err);
+    resolve();
+  }));
 }
